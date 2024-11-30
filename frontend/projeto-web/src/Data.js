4a663ccda87db15ee1f0251b2e545050eb89dc15
lukas.resgalla <lukasresgalla@gmail.com>
2024-11-30 20:03:41 -0300

    Criando data mock

---
+++ frontend/projeto-web/src/Data.js
@@ -0,0 +1,9 @@
+import { text } from "express";
+
+export const news = {
+    title: 'Notícia 1',
+    text: 'Texto da notícia 1',
+    Image: 'https://www.oficinadanet.com.br/media/post/25197/330/images.jpg',
+    likes: 0,
+    comments: 0,
+}
