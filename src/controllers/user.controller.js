ada719ef5d1023b4b95a2f0adc639ef4b24dea13
Kado.cbc <kado.cbc@gmail.com>
2024-11-30 17:37:07 -0300

    Criando conexão com banco de dados

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,22 @@
+const UserService = require('../services/user.service')
 
-const create = (req, res) => {
+const create = async (req, res) => {
     const {name, username, email, password, avatar, background} = req.body;
 
     if (!name || !username || !email || !password || !avatar || !background) {
         res.status(400).send({message: "preencha todos os campos para se registrar"})
     }
 
+    const user = await UserService.create(req.body);
+
+    if (!user){
+        return res.status(400).send({ message: "erro ao criar usuario"});
+    }
+
     res.status(201).send({
         message: "Usuario criado com sucesso",
         user: {
+            id: user._id,
             name: name,
             username: username,
             email: email,
---
+++ src/database/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectcDatabase = () => {
+    console.log("Espere conectando com o banco")
+
+    mongoose.connect("mongodb+srv://web:5646@cluster0.6uwsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", 
+    )
+    .then(() => console.log("MongoDB Atlas Connected"))
+    .catch((error) => console.log(error));
+};
+
+module.exports = connectcDatabase;
