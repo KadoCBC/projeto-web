9732eca8dc90f2239c8522144a0adcb5232a9bb7
Kado.cbc <kado.cbc@gmail.com>
2024-11-30 14:45:05 -0300

    primeira requisição; rota user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,22 @@
-const soma = (req, res) => {
-    const soma = 100 + 1;
 
-    res.send({soma: soma});
+const create = (req, res) => {
+    const {name, username, email, password, avatar, background} = req.body;
+
+    if (!name || !username || !email || !password || !avatar || !background) {
+        res.status(400).send({message: "preencha todos os campos para se registrar"})
+    }
+
+    res.status(201).send({
+        message: "Usuario criado com sucesso",
+        user: {
+            name: name,
+            username: username,
+            email: email,
+            avatar: avatar,
+            background: background,
+        },
+    });
 };
 
-module.exports = { soma };
+
+module.exports = {create};
