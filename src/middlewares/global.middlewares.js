94e916fcd6a4a55ae6732499c4a5acca1d76cf78
davibomf <58120196+davibomf@users.noreply.github.com>
2024-12-01 16:26:11 -0300

    Adicionado o try, catch
    Adicionado o try, catch em ValidUser

--- src/middlewares/global.middlewares.js
+++ src/middlewares/global.middlewares.js
@@ -5,8 +5,8 @@ const mongoose = require("mongoose")
 const validId = (req, res, next) => {
     const id = req.params.id;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(400).send({ message: "ID inválido"})
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "ID inválido" })
     }
 
     next();
@@ -14,20 +14,24 @@ const validId = (req, res, next) => {
 
 //Valida o usuário pelo Id
 const validUser = async (req, res, next) => {
-    const id = req.params.id;
+    try{
+        const id = req.params.id;
 
-    const user = await userService.findByIdService(id);
+        const user = await userService.findByIdService(id);
 
-    if (!user) {
-        return res.status(400).send({ message: "Id de usuario não encontrado" });
+        if (!user) {
+            return res.status(400).send({ message: "Id de usuario não encontrado" });
+        }
+
+        //A requisição que vai ser enviada para a próxima função vai ter id e user
+        req.id = id;
+        req.user = user;
+
+        //Vai para a proxima função da chamada em user.route
+        next();
+    } catch (err) {
+        res.status(500).send({ message: 'Falha ao conectar ao banco de dados' });
     }
-
-    //A requisição que vai ser enviada para a próxima função vai ter id e user
-    req.id = id;
-    req.user = user;
-
-    //Vai para a proxima função da chamada em user.route
-    next();
 };
 
-module.exports = {validId, validUser};
+module.exports = { validId, validUser };
