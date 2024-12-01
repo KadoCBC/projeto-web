98a67f412bf25ae4afa7ba69c3015c9c2cb68cb5
Kado.cbc <kado.cbc@gmail.com>
2024-11-30 21:26:05 -0300

    Criando rotas get e patch para user e validações

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,17 @@
-const UserService = require('../services/user.service')
+const userService = require('../services/user.service')
+const mongoose = require("mongoose")
 
 const create = async (req, res) => {
-    const {name, username, email, password, avatar, background} = req.body;
+    const { name, username, email, password, avatar, background } = req.body;
 
     if (!name || !username || !email || !password || !avatar || !background) {
-        res.status(400).send({message: "preencha todos os campos para se registrar"})
+        res.status(400).send({ message: "preencha todos os campos para se registrar" })
     }
 
-    const user = await UserService.create(req.body);
+    const user = await userService.createService(req.body);
 
-    if (!user){
-        return res.status(400).send({ message: "erro ao criar usuario"});
+    if (!user) {
+        return res.status(400).send({ message: "erro ao criar usuario" });
     }
 
     res.status(201).send({
@@ -26,5 +27,60 @@ const create = async (req, res) => {
     });
 };
 
+const findAll = async (req, res) => {
+    const users = await userService.findAllService();
 
-module.exports = {create};
+    if (users.length === 0) {
+        return res.status(400).send({ message: "Não há usuarios registrados" });
+    }
+    res.send(users);
+};
+
+const findById = async (req, res) => {
+    const id = req.params.id;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).send({ message: "ID inválido"})
+    }
+
+    const user = await userService.findByIdService(id);
+
+    if (!user) {
+        return res.status(400).send({ message: "Id de usuario não encontrado" });
+    }
+
+    res.send(user);
+};
+
+const update = async (req, res) => {
+    const { name, username, email, password, avatar, background } = req.body;
+
+    if (!name && !username && !email && !password && !avatar && !background) {
+        res.status(400).send({ message: "Preecha pelo menos um campo para atualizar"});
+    }
+    const id = req.params.id;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).send({ message: "ID inválido"});
+    }
+
+    const user = await userService.findByIdService(id);
+
+    if (!user) {
+        return res.status(400).send({ message: "Id de usuario não encontrado" });
+    }
+    
+    await userService.updateService(
+        id,
+        name,
+        username,
+        email,
+        password,
+        avatar,
+        background
+    );
+
+    res.send({ message: "Usuario foi atualizado com Sucesso!"})
+};
+
+module.exports = { create, findAll, findById, update};
