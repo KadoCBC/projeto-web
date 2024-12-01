735b3e3a38838ec6fcddbdd26868b8c12a55d536
davibomf <58120196+davibomf@users.noreply.github.com>
2024-12-01 16:25:07 -0300

    Adicionado o try, catch
    Adicionado o try, catch nas funções

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,65 +2,81 @@ const userService = require('../services/user.service')
 const mongoose = require("mongoose")
 
 const create = async (req, res) => {
-    const { name, username, email, password, avatar, background } = req.body;
+    try {
+        const { name, username, email, password, avatar, background } = req.body;
 
-    if (!name || !username || !email || !password || !avatar || !background) {
-        res.status(400).send({ message: "preencha todos os campos para se registrar" })
+        if (!name || !username || !email || !password || !avatar || !background) {
+            res.status(400).send({ message: "preencha todos os campos para se registrar" })
+        }
+
+        const user = await userService.createService(req.body);
+
+        if (!user) {
+            return res.status(400).send({ message: "erro ao criar usuario" });
+        }
+
+        res.status(201).send({
+            message: "Usuario criado com sucesso",
+            user: {
+                id: user._id,
+                name: name,
+                username: username,
+                email: email,
+                avatar: avatar,
+                background: background,
+            },
+        });
+    } catch (err) {
+        res.status(500).send({ message: 'Falha ao conectar ao banco de dados' });
     }
-
-    const user = await userService.createService(req.body);
-
-    if (!user) {
-        return res.status(400).send({ message: "erro ao criar usuario" });
-    }
-
-    res.status(201).send({
-        message: "Usuario criado com sucesso",
-        user: {
-            id: user._id,
-            name: name,
-            username: username,
-            email: email,
-            avatar: avatar,
-            background: background,
-        },
-    });
 };
 
 const findAll = async (req, res) => {
-    const users = await userService.findAllService();
+    try {
+        const users = await userService.findAllService();
 
-    if (users.length === 0) {
-        return res.status(400).send({ message: "Não há usuarios registrados" });
+        if (users.length === 0) {
+            return res.status(400).send({ message: "Não há usuarios registrados" });
+        }
+        res.send(users);
+    } catch (err) {
+        res.status(500).send({ message: 'Falha ao conectar ao banco de dados' });
     }
-    res.send(users);
 };
 
 const findById = async (req, res) => {
-    const user = req.user;
-    res.send(user);
+    try {
+        const user = req.user;
+        res.send(user);
+    } catch (err){
+        res.status(500).send({ message: 'Falha ao conectar ao banco de dados' });
+    }
 };
 
 const update = async (req, res) => {
-    const { name, username, email, password, avatar, background } = req.body;
+    try {
+        const { name, username, email, password, avatar, background } = req.body;
 
-    if (!name && !username && !email && !password && !avatar && !background) {
-        res.status(400).send({ message: "Preecha pelo menos um campo para atualizar"});
+        if (!name && !username && !email && !password && !avatar && !background) {
+            res.status(400).send({ message: "Preecha pelo menos um campo para atualizar" });
+        }
+
+        const { id, user } = req;
+
+        await userService.updateService(
+            id,
+            name,
+            username,
+            email,
+            password,
+            avatar,
+            background
+        );
+
+        res.send({ message: "Usuario foi atualizado com Sucesso!" })
+    } catch (err){
+        res.status(500).send({ message: 'Falha ao conectar ao banco de dados' });
     }
-
-    const {id, user} = req;
-    
-    await userService.updateService(
-        id,
-        name,
-        username,
-        email,
-        password,
-        avatar,
-        background
-    );
-
-    res.send({ message: "Usuario foi atualizado com Sucesso!"})
 };
 
-module.exports = { create, findAll, findById, update};
+module.exports = { create, findAll, findById, update };
