c39cc2402d3f35fb5b83dc47dc9b8816ce768e8c
Kado.cbc <kado.cbc@gmail.com>
2024-12-05 22:14:25 -0300

    Criando a paginação

--- src/services/news.service.js
+++ src/services/news.service.js
@@ -2,9 +2,13 @@ import News from "../models/News.js";
 
 const createService = (body) => News.create(body);
 
-const findAllService = () => News.find();
+//sort com id -1 para pegar as ultimas noticias postadas
+const findAllService = (offset, limit) => News.find().sort({_id: -1}).skip(offset).limit(limit).populate("user");
+
+const countNews = () => News.countDocuments();
 
 export {
     createService,
-    findAllService
-}
+    findAllService,
+    countNews
+};
