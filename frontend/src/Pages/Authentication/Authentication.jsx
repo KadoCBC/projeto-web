7630942bda07c89e7dead3d7f69be2c4f1a96a74
artbiehl <arthurbiehl21@gmail.com>
2024-12-07 15:27:25 -0300

    Add styledcomponents na tela de login

--- frontend/src/Pages/Authentication/Authentication.jsx
+++ frontend/src/Pages/Authentication/Authentication.jsx
@@ -1,7 +1,14 @@
+import { AuthContainer, Section } from "./AuthenticationStyled";
+
 export function Authentication() {
     return (
-        <div>
-            <h1>Authentication</h1>
-        </div>
+        <AuthContainer>
+            <Section type="signin"> {/* tipo de autenticacao */}
+                <h2>Entrar</h2>
+            </Section>
+            <Section type="signup">
+                <h2>Cadastrar</h2>
+            </Section>                
+        </AuthContainer>
     );
 }
